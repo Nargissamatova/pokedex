654f83689908299781cf4fa6c316843e442b9d66
Nargis <nargissamatova16@gmail.com>
2024-03-15 21:36:44 +0200

    updated

--- pokemon/main.js
+++ pokemon/main.js
@@ -22,13 +22,22 @@ const fetchData = async () => {
 
 // PROMISE VERSION
 let pokemons = [];
+
 const fetchData = () => {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=500&offset=0")
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
     .then((response) => response.json())
     .then((json) => {
-      pokemons = json.results;
-      displayData(pokemons);
-    });
+      const fetches = json.results.map((item) => {
+        return fetch(item.url).then((res) => res.json());
+      });
+
+      Promise.all(fetches).then((data) => {
+        pokemons = data;
+        displayData(pokemons);
+        console.log(pokemons);
+      });
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 };
 
 fetchData();
@@ -39,21 +48,65 @@ const displayData = (data) => {
 
   data.forEach((pokemon) => {
     const pokemonCard = document.createElement("div");
-    pokemonCard.innerHTML = `<h2>${pokemon.name}</h2>`;
+    const imageUrl =
+      pokemon.sprites.other.dream_world.front_default ??
+      pokemon.sprites.other["official-artwork"].front_default ??
+      "assets/sadpokemon.jpeg";
+
+    const isFavorite = localStorage.getItem(pokemon.name) === "true";
+    const favoriteText = isFavorite ? "Unmark favorite" : "Mark favorite";
+
+    pokemonCard.innerHTML = `<h2>${pokemon.name}</h2>
+    <img src="${imageUrl}"/>
+    <div class="card">
+      <p>Weight: ${pokemon.weight / 10} kg</p>
+      <p>Height: ${pokemon.height / 10} m</p>
+      <button id="favButton" data-name=${pokemon.name}> ${favoriteText}</button>
+    </div>
+    `;
     container.appendChild(pokemonCard);
   });
+  addFavorites();
 };
 
-// Search function
+const toggleFavorite = (e) => {
+  const pokemonName = e.target.getAttribute("data-name");
+  const isFavorite = localStorage.getItem(pokemonName) === "true";
+  localStorage.setItem(pokemonName, !isFavorite);
+};
 
-const searchPokemons = (wateeveeee) => {
+const addFavorites = () => {
+  document
+    .querySelectorAll("#favButton")
+    .forEach((button) => button.addEventListener("click", toggleFavorite));
+};
+
+const debounce = (func, delay) => {
+  let debounceTimer;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+
+const searchPokemons = debounce((searchInput) => {
+  console.log("search triggered");
   const filteredData = pokemons.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(wateeveeee.toLowerCase())
+    pokemon.name.toLowerCase().includes(searchInput.toLowerCase())
   );
   displayData(filteredData);
-};
+}, 300);
 
-// searchInput.addEventListener("input", searchNames); or console.log(e.target.values) OR
 document.querySelector("#search").addEventListener("input", (e) => {
   searchPokemons(e.target.value);
 });
+
+// Show favorites
+const displayfavorites = () => {
+  console.log();
+};
+document
+  .querySelector("#showFavorites")
+  .addEventListener("click", displayfavorites);
